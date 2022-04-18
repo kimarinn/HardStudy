79f047a63ee8ac1e62a6b65a29d5f1537d3858a1
kimarin <rinak@naver.com>
2022-04-18 22:20:01 +0900

    프로젝트 생성

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,29 @@
 <template>
+<div>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <div id="example-1">
+    <input type="button" value="Test" onclick="alert('테스트')">
+  </div>
+  <div id="example-2">
+    <button @click="counting">Count</button>
+    <p>버튼을 클릭한 횟수: {{ counter }}</p>
+  </div>
+</div>
 </template>
+<script>
+export default {
+  name: 'example-2',
+  data () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    counting () {
+      this.counter++
+    }
+  }
+}
+</script>
